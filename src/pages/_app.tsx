ec860ea087ca8baea9278a52ece4f565718c7939
0xbbadbeef <awesome_dude6@live.com>
2024-01-01 22:32:29 +1100

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "~/styles/global.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
+    </>
+  );
 }
